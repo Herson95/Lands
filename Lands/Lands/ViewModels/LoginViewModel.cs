6604a096a206e1854c209f975c14f3a519356385
Herson95 <38219534+Herson95@users.noreply.github.com>
2018-04-14 22:34:09 -0600

    Login View Model

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+    }
+}
