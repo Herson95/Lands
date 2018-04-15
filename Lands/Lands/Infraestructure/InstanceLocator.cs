799f21aed0b25529b03877eeb6e664c31c6eecfe
Herson95 <38219534+Herson95@users.noreply.github.com>
2018-04-14 18:56:24 -0600

    Lands

---
+++ Lands/Lands/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,19 @@
+﻿namespace Lands.Infraestructure
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        } 
+        #endregion
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
