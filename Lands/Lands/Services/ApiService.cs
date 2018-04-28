c907e57dbc7fa07215c6d4cde03b8aabb7554b78
Herson95 <38219534+Herson95@users.noreply.github.com>
2018-04-27 21:46:56 -0600

    ApiServices

--- Lands/Lands/Services/ApiService.cs
+++ Lands/Lands/Services/ApiService.cs
@@ -118,14 +118,11 @@ namespace Lands.Services
         public async Task<Response> GetList<T>(
             string urlBase,
             string servicePrefix,
-            string controller,
-            string tokenType,
-            string accessToken)
+            string controller)
         {
             try
             {
                 var client = new HttpClient();
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(tokenType, accessToken);
                 client.BaseAddress = new Uri(urlBase);
                 var url = string.Format("{0}{1}",servicePrefix,controller);
                 var response = await client.GetAsync(url);
@@ -161,6 +158,7 @@ namespace Lands.Services
             }
         }
 
+
         public async Task<Response> GetList<T>(
             string urlBase,
             string servicePrefix,
@@ -190,6 +188,52 @@ namespace Lands.Services
                 }
 
                 var result = await response.Content.ReadAsStringAsync();
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Message = "Ok",
+                    Result = list,
+
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+
+                };
+            }
+        }
+
+        public async Task<Response> GetList<T>(
+            string urlBase,
+            string servicePrefix,
+            string controller,
+            string tokenType,
+            string accessToken)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(tokenType, accessToken);
+                client.BaseAddress = new Uri(urlBase);
+                var url = string.Format("{0}{1}", servicePrefix, controller);
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+
+                    };
+                }
+
                 var list = JsonConvert.DeserializeObject<List<T>>(result);
                 return new Response
                 {
@@ -406,9 +450,6 @@ namespace Lands.Services
         }
 
 
-
-
     }
-
 
 }
